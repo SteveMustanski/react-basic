adf0bffeed6e93470da6fba397b77351de686089
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-02 11:51:57 -0500

    modified incrementScore to use arrow function syntax so it does have to be explicitly bound to Counter

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -54,7 +54,7 @@ class Counter extends React.Component {
     score: 0
   };
 
-  incrementScore() {
+  incrementScore = () => {
     this.setState({
       score: this.state.score + 1
     });
@@ -65,7 +65,7 @@ class Counter extends React.Component {
       <div className='counter'>
         <button className='counter-action decrement'> - </button>
         <span className='counter-score'>{this.state.score}</span>
-        <button className='counter-action increment' onClick={() => this.incrementScore()}> + </button>
+        <button className='counter-action increment' onClick={this.incrementScore}> + </button>
       </div>
     );
   }
