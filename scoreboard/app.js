c6a4c37d0dd8c1b0270ca459490948824486cbd5
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-02 10:33:28 -0500

    add props to player and counter to dynamically pass values to the components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -8,23 +8,23 @@ const Header = (props) => {
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className='player'>
       <span className='player-name'>
-        Steve
+        {props.name}
       </span>
 
-      <Counter />
+      <Counter score={props.score}/>
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className='counter'>
       <button className='counter-action decrement'> - </button>
-      <span className='counter-score'>35</span>
+      <span className='counter-score'>{props.score}</span>
       <button className='counter-action increment'> + </button>
     </div>
   )
@@ -33,10 +33,13 @@ const Counter = () => {
 const App = () => {
   return (
   <div className="scoreboard">
-    <Header title="Scoreboard" totalPlayers={1}/>
+    <Header title="Scoreboard"
+     totalPlayers={1}/>
 
     {/* start of player list */}
-    <Player />
+    <Player name='Steve'
+      score={35}
+    />
   </div>
   )
 }
