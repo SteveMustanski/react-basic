0c4b4e0c24e8743d32fcd7195e863ab64d95c159
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-01 21:10:16 -0500

    created Player, Counter, and App components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -8,7 +8,40 @@ const Header = () => {
   );
 }
 
-ReactDOM.render( 
-  <Header />,
+const Player = () => {
+  return (
+    <div className='player'>
+      <span className='player-name'>
+        Steve
+      </span>
+
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => {
+  return (
+    <div className='counter'>
+      <button className='counter-action decrement'> - </button>
+      <span className='counter-score'>35</span>
+      <button className='counter-action increment'> + </button>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+  <div className="scoreboard">
+    <Header />
+
+    {/* start of player list */}
+    <Player />
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
